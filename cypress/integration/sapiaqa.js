7cd7501bef1b7df518889e3c754c8e17000f7f36
BelgicaJanet <esfingebdltg@gmail.com>
2020-05-19 08:59:48 -0500

    -1

--- cypress/integration/sapiaqa.js
+++ cypress/integration/sapiaqa.js
@@ -7,8 +7,8 @@ describe("sapia app", () => {
         cy.get('.form-group > :nth-child(3)').click();
         cy.get('#defaultUnchecked4').check({force: true});
         cy.get('.btn').click();
-        cy.get('.flex-center > :nth-child(2)').click();
-        /*cy.get('#defaultUnchecked10').check({force: true});
+        /*cy.get('.flex-center > :nth-child(2)').click();
+        cy.get('#defaultUnchecked10').check({force: true});
         cy.get('.btn').click();
         cy.get('.flex-center > :nth-child(2)').click();
         cy.get('.input-checkbox').type("San Martin de Porres{enter}");
